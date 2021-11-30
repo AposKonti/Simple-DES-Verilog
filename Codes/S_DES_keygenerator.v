f92796d8f020181d0e2b97b0b4b96793c2ec8764
unknown <delpiero1999@gmail.com>
2021-11-30 19:39:20 +0200

    Subkeys generator

---
+++ Codes/S_DES_keygenerator.v
@@ -0,0 +1,8 @@
+module S_DES_keygenerator(key, subkeys);
+
+input [9:0] key;
+output [15:0] subkeys;
+
+assign subkeys[15:0] = {key[1],key[6],key[2],key[7],key[4],key[5],key[9],key[0],key[9],key[3],key[1],key[6],key[2],key[7],key[0],key[4]};
+
+endmodule
