745d7ea15139c31f4b1e06704470a3b240bfb5ab
unknown <delpiero1999@gmail.com>
2021-12-01 14:23:01 +0200

    Subkeys Generator fix

--- Codes/S_DES_keygenerator.v
+++ Codes/S_DES_keygenerator.v
@@ -3,6 +3,6 @@ module S_DES_keygenerator(key, subkeys);
 input [9:0] key;
 output [15:0] subkeys;
 
-assign subkeys[15:0] = {key[1],key[6],key[2],key[7],key[4],key[5],key[9],key[0],key[9],key[3],key[1],key[6],key[2],key[7],key[0],key[4]};
+assign subkeys[15:0] = {key[2],key[7],key[4],key[5],key[0],key[2],key[9],key[1],key[1],key[3],key[9],key[6],key[2],key[7],key[0],key[4]};
 
 endmodule
