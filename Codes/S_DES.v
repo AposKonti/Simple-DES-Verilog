a74229fe16f038981630bf88d2e9fe86c4cbe6d4
unknown <delpiero1999@gmail.com>
2021-12-11 11:23:21 +0200

    Bug in S_DES_Fk

--- Codes/S_DES.v
+++ Codes/S_DES.v
@@ -1,49 +1,29 @@
 module S_DES(plaintext, key, S0, S1, ciphertext);
 
-input [7:0] plaintext; //The plain text
-input [9:0] key; //10-bit Key
-input [31:0] S0, S1; //S-Boxes
-output [7:0] ciphertext; //The resulted cipher text
+input [7:0] plaintext;                                                  //The plain text
+input [9:0] key;                                                        //10-bit Key
+input [31:0] S0, S1;                                                    //S-Boxes
+output [7:0] ciphertext;                                                //The resulted cipher text
 
-wire [7:0] IP; //IP <= Initial Permutation //DONT NEED - WILL BE REMOVED
-wire [7:0] EP; //EP <= Expand and Permutate //DONT NEED - WILL BE REMOVED
-wire [7:0] EPxorK1; // EP XOR K1 //DONT NEED - WILL BE REMOVED
-wire [1:0] row0, col0, row1, col1; //DONT NEED - WILL BE REMOVED
-wire [4:0] index0, index1;
-wire [3:0] S0S1; //DONT NEED - WILL BE REMOVED
-wire [3:0] P4; //DONT NEED - WILL BE REMOVED
-wire [3:0] PTxorP4; //DONT NEED - WILL BE REMOVED
-wire [7:0] block1; //DONT NEED - WILL BE REMOVED
+wire [7:0] IP, IP_1, K1, K2;                                            //IP <= Initial Permutation, K1 <= Key 1, K2 <= Key 2
+wire [7:0] res1,sw,res2;
 
-assign IP[7:4] = {plaintext[6],plaintext[2],plaintext[5],plaintext[7]}; //DONT NEED - WILL BE REMOVED 
-assign IP[3:0] = {plaintext[4],plaintext[0],plaintext[3],plaintext[1]}; //DONT NEED - WILL BE REMOVED
+//------------------------KEY GENERATION------------------------
+assign K1 = {key[1],key[3],key[9],key[6],key[2],key[7],key[0],key[4]};  //K1 generation
+assign K2 = {key[2],key[7],key[4],key[5],key[0],key[2],key[9],key[1]};  //K2 generation
+//--------------------------------------------------------------
 
-assign EP[7:4] = {plaintext[1],plaintext[4],plaintext[0],plaintext[3]}; //DONT NEED - WILL BE REMOVED
-assign EP[3:0] = {plaintext[0],plaintext[3],plaintext[1],plaintext[4]}; //DONT NEED - WILL BE REMOVED
+//-----------------------S-DES ENCRYPTION-----------------------
+assign IP[7:4] = {plaintext[6],plaintext[2],plaintext[5],plaintext[7]}; //Initial Permutation
+assign IP[3:0] = {plaintext[4],plaintext[0],plaintext[3],plaintext[1]}; //Initial Permutation
+S_DES_Fk f1(IP[7:0], K1[7:0], S0[31:0], S1[31:0], res1[7:0]);           //First Fk
+assign sw[7:4] = res1[3:0];                                             //Swap
+assign sw[3:0] = res1[7:4];                                             //Swap
+S_DES_Fk f2(sw[7:0], K2[7:0], S0[31:0], S1[31:0], res2[7:0]);           //Second Fk
+assign IP_1[7:4] = {res2[4], res2[7], res2[5], res2[3]};                //IP^(-1)
+assign IP_1[3:0] = {res2[1], res2[6], res2[0], res2[2]};                //IP^(-1)
+//--------------------------------------------------------------
 
-assign EPxorK1[7:6] ={plaintext[1] ^ key[1], plaintext[4] ^ key[3]}; //DONT NEED - WILL BE REMOVED
-assign EPxorK1[5:4] ={plaintext[0] ^ key[9], plaintext[3] ^ key[6]}; //DONT NEED - WILL BE REMOVED
-assign EPxorK1[3:2] ={plaintext[0] ^ key[2], plaintext[3] ^ key[7]}; //DONT NEED - WILL BE REMOVED
-assign EPxorK1[1:0] ={plaintext[1] ^ key[0], plaintext[4] ^ key[4]}; //DONT NEED - WILL BE REMOVED
-
-assign row0[1:0] = {plaintext[1] ^ key[1],plaintext[3] ^ key[6]}; //DONT NEED - WILL BE REMOVED
-assign col0[1:0] = {plaintext[4] ^ key[3],plaintext[0] ^ key[9]}; //DONT NEED - WILL BE REMOVED
-assign row1[1:0] = {plaintext[0] ^ key[2],plaintext[4] ^ key[4]}; //DONT NEED - WILL BE REMOVED
-assign col1[1:0] = {plaintext[3] ^ key[7],plaintext[1] ^ key[0]}; //DONT NEED - WILL BE REMOVED
-
-assign index0[4:0] = {plaintext[1] ^ key[1],plaintext[3] ^ key[6],plaintext[4] ^ key[3],plaintext[0] ^ key[9],1'b0}; 
-assign index1[4:0] = {plaintext[0] ^ key[2],plaintext[4] ^ key[4],plaintext[3] ^ key[7],plaintext[1] ^ key[0],1'b0}; 
-
-assign S0S1[3:0] = {S0[index0+1],S0[index0],S1[index0+1],S1[index0]}; //DONT NEED - WILL BE REMOVED
-
-assign P4[3:0] = {S0[index0],S1[index0],S1[index0+1],S0[index0+1]}; //DONT NEED - WILL BE REMOVED
-
-assign PTxorP4[3:2] = {plaintext[6] ^ S0[index0], plaintext[2] ^ S1[index0]}; //DONT NEED - WILL BE REMOVED
-assign PTxorP4[1:0] = {plaintext[5] ^ S1[index0+1], plaintext[7] ^ S0[index0+1]}; //DONT NEED - WILL BE REMOVED
-
-assign block1[7:4] = {plaintext[4],plaintext[0],plaintext[3],plaintext[1]}; //DONT NEED - WILL BE REMOVED
-assign block1[3:0] = {plaintext[6] ^ S0[index0],plaintext[2] ^ S1[index0],plaintext[5] ^ S1[index0+1],plaintext[7] ^ S0[index0+1]} ; //DONT NEED - WILL BE REMOVED
-
-assign ciphertext[7:0] = block1[7:0];
+assign ciphertext[7:0] = IP_1[7:0];
 
 endmodule
--- Codes/S_DES_keygenerator.v
+++
@@ -1,8 +0,0 @@
-module S_DES_keygenerator(key, subkeys);
-
-input [9:0] key;
-output [15:0] subkeys;
-
-assign subkeys[15:0] = {key[2],key[7],key[4],key[5],key[0],key[2],key[9],key[1],key[1],key[3],key[9],key[6],key[2],key[7],key[0],key[4]};
-
-endmodule
