507068df96bbcee7b886a065e4a7d2cf54bda607
unknown <delpiero1999@gmail.com>
2021-12-13 11:17:24 +0200

    All codes final and working

--- Codes/S_DES.v
+++ Codes/S_DES.v
@@ -1,29 +1,14 @@
-module S_DES(plaintext, key, S0, S1, ciphertext);
+module S_DES(plaintext, key, S0, S1, ciphertext, decryptedtext);
 
 input [7:0] plaintext;                                                  //The plain text
 input [9:0] key;                                                        //10-bit Key
 input [31:0] S0, S1;                                                    //S-Boxes
-output [7:0] ciphertext;                                                //The resulted cipher text
+output [7:0] ciphertext, decryptedtext;                                 //The resulted cipher text
 
-wire [7:0] IP, IP_1, K1, K2;                                            //IP <= Initial Permutation, K1 <= Key 1, K2 <= Key 2
-wire [7:0] res1,sw,res2;
+wire [7:0] ct;
 
-//------------------------KEY GENERATION------------------------
-assign K1 = {key[1],key[3],key[9],key[6],key[2],key[7],key[0],key[4]};  //K1 generation
-assign K2 = {key[2],key[7],key[4],key[5],key[0],key[2],key[9],key[1]};  //K2 generation
-//--------------------------------------------------------------
-
-//-----------------------S-DES ENCRYPTION-----------------------
-assign IP[7:4] = {plaintext[6],plaintext[2],plaintext[5],plaintext[7]}; //Initial Permutation
-assign IP[3:0] = {plaintext[4],plaintext[0],plaintext[3],plaintext[1]}; //Initial Permutation
-S_DES_Fk f1(IP[7:0], K1[7:0], S0[31:0], S1[31:0], res1[7:0]);           //First Fk
-assign sw[7:4] = res1[3:0];                                             //Swap
-assign sw[3:0] = res1[7:4];                                             //Swap
-S_DES_Fk f2(sw[7:0], K2[7:0], S0[31:0], S1[31:0], res2[7:0]);           //Second Fk
-assign IP_1[7:4] = {res2[4], res2[7], res2[5], res2[3]};                //IP^(-1)
-assign IP_1[3:0] = {res2[1], res2[6], res2[0], res2[2]};                //IP^(-1)
-//--------------------------------------------------------------
-
-assign ciphertext[7:0] = IP_1[7:0];
+S_DES_encryption SDESen(plaintext[7:0], key[9:0], S0[31:0], S1[31:0], ct[7:0]);
+assign ciphertext[7:0] = ct[7:0];
+S_DES_decryption SDESde(ct[7:0], key[9:0], S0[31:0], S1[31:0], decryptedtext[7:0]);  
 
 endmodule
--- Codes/S_DES_Fk.v
+++ Codes/S_DES_Fk.v
@@ -9,7 +9,7 @@ wire [4:0] index0, index1;
 assign index0[4:0] = {IP[0] ^ key[7], IP[1] ^ key[4], IP[3] ^ key[6], IP[2] ^ key[5], 1'b0}; 
 assign index1[4:0] = {IP[2] ^ key[3], IP[3] ^ key[0], IP[1] ^ key[2], IP[0] ^ key[1], 1'b0}; 
 
-assign res[7:4] = {IP[7] ^ S0[index0], IP[6] ^ S1[index0], IP[5] ^ S1[index0+1], IP[4] ^ S0[index0+1]}; 
+assign res[7:4] = {IP[7] ^ S0[index0], IP[6] ^ S1[index1], IP[5] ^ S1[index1+1], IP[4] ^ S0[index0+1]}; 
 assign res[3:0] = IP[3:0] ;
 
 endmodule
